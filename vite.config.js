e33555d899ad9dc662ea300319b9d5ab51ac240f
Maraysa <maraysa1515@gmail.com>
2025-03-03 10:44:46 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/maraysamacedo.github.io/',
+  base: '/maraysamacedo',
   plugins: [vue()],
 })
